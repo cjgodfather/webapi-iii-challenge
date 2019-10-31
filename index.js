23e17764583a2974b08baf6c9bec93fb45d59ae7
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-31 16:13:21 -0400

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server.js");
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`\n* Server Running on ${port} *\n`);
